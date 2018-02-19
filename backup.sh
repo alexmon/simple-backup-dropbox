b8150f0153e12d3093b126d6d7d42b44d60e0213
Alexandros Monastiriotis <alex.monastiriotis@gmail.com>
2018-02-19 12:51:29 +0200

    update backup.sh, commit before pushing to Dropbox

--- backup.sh
+++ backup.sh
@@ -1,19 +1,23 @@
 ACCESS_TOKEN="DROPBOX_GENERATED_ACCESS_TOKEN"
 
+# commit changes on source folder, push to stage repo
+cd /var/www/my-site
+git add . -A
+git commit -am "`date +'%Y-%m-%d %H:%M'`"
+git push stage master
+
+# create temp folder
 mkdir -p /tmp/my-site/splits
-
 cd /tmp/my-site
-
 echo > log.txt
-
 rm my-site.tar
-
 rm ./splits/my-site.tar.*
 
+# create archive of stage repo, make splits
 tar -cvf my-site.tar  /var/repos/my-site.git
-
 split --bytes=50M my-site.tar ./splits/my-site.tar.
 
+# push splits to Dropbox
 cd /tmp/my-site/splits
 
 for f in my-site.tar.*; do
